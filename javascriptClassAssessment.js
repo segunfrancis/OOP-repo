3f8a7f5811e42333841c34a6617ce78180fd2145
Segun Francis <francis4dblues@gmail.com>
2016-05-19 06:39:47 +0100

    Add javascriptClassAssessment.js file

--- javascriptClassAssessment.js
+++ javascriptClassAssessment.js
@@ -3,34 +3,48 @@ function NotesApplication(author) {
 	this.notes =[];
 	
 	this.create = function(note_content) {
-		return this.notes.push('note_content');
+		return this.notes.push([this.notes.length + 1,note_content]);
 	};
 	
 	this.listNotes = function() {
-		this.note_id = function() {
+
 		for (var i = 0; i < this.notes.length; i++) {
-			return i; 
+
+			console.log("Note ID: " + this.notes[i][0]);
+	  		console.log(this.notes[i][1]);
+	  		console.log("By author: " + author);
 		}
-		};
-		console.log("Note ID: " + this.note_id());
-  		console.log(this.notes);
-  		console.log("By author: " + author);
-  		};
+	};
 	
 	this.get = function(note_id) {
-  		return this.notes;
+  		for (var i = 0; i < this.notes.length; i++) {
+			if(this.notes[i][0] === note_id) {
+				return this.notes[i][1];
+			}
+		}
+  		
     };
 
   	this.search = function(search_text) {
-  		for(var x in this.notes) {
-  			if(search_text === x) {
-  				console.log("Showing results for search " + search_text);
-  				console.log("Note ID: " + this.note_id);
-  				console.log(this.notes);
-  				console.log("By author: " + author);
-  			} else {
-  				console.log("Not Found!");
+  		console.log("Showing results for search " + '"'+search_text+'"');
+  		var result = [];
+  		for(var x = 0; x < this.notes.length;x++) {
+  			
+  			if(this.notes[x][1].includes(search_text)) {
+  				result.push(this.notes[x]);
+  			} 
+  			
+		}
+		
+		if(result.length > 0) {
+			for(var j = 0; j < result.length;j++) {
+				console.log();
+	  			console.log("Note ID: " + result[j][0]);
+	  			console.log(result[j][1]);
+	  			console.log("By author: " + author);
 			}
+		} else {
+			console.log("No match found!");
 		}
 	};
 
@@ -43,14 +57,12 @@ function NotesApplication(author) {
 
 	this.edit = function(note_id, new_content) {
 		note_id = new_content;
-		return note_id
+		return note_id;
 	};
 }
 
-var mike = new NotesApplication('William');
-mike.create("It's been good so far");
-mike.listNotes();
-mike.get("Hey");
-mike.search("why");
-mike.edit("oh", "what");
-mike.delete(0)
+var okoro = new NotesApplication("okoro");
+okoro.create("This is a note");
+okoro.create("Second note");
+console.log(okoro.get(2));
+okoro.search('ast');
