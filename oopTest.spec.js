6f1da8967b8d21a5b574d1d61924dd7e56741724
Segun Francis <francis4dblues@gmail.com>
2016-05-19 08:38:27 +0100

    Add oopTest.spec.js file

--- oopTest.spec.js
+++ oopTest.spec.js
@@ -8,14 +8,18 @@ describe("Instance of the class", function () {
 
 
 describe("Adds to an empty list", function() {
-	var mike = new NotesApplication("mike");
+	
 	it("Should push note_content to mike.notes", function() {
-		expect(typeof mike.create()).toBe(typeof(array));
+		var mike = new NotesApplication("mike");
+		mike.create("This is note one");
+		expect(typeof mike.notes).toBe('object');
 	});
-	it("Should push note_content to mike.notes", function() {
-		expect(mike.create()).toContain([]);
+	it("Should push cane to mike.notes", function() {
+		var mike = new NotesApplication("mike");
+		expect(mike.notes.length).toEqual(0);
 	});
 	it("Should return error when passed an empty string", function() {
+		var mike = new NotesApplication("mike");
 		
 		expect(mike.create("")).toBe("error");
 	});
@@ -24,20 +28,28 @@ describe("Adds to an empty list", function() {
 
 describe("Gets note ID", function() {
 	it("Should return string", function() {
-		expect(typeof this.get(note_id)).toBe(typeof(string)); 
+		var mike = new NotesApplication("mike");
+		mike.create('This is second note');
+		expect(typeof mike.get(1)).toBe('string'); 
+		expect(mike.get(1)).toBe('This is second note'); 
 	});	
 });
 
 describe("Search text", function() {
 	it("Should take a string", function() {
+		var mike = new NotesApplication("mike");
+		mike.create('This is second note');
+
 		expect(typeof search_text).toBe(typeof(string))
 	});
-	it("throws an error", function() {
-		expect(search(2)).toThrow(new Error("unrecognized"));
+	it("Should return error", function() {
+		var mike = new NotesApplication("mike");
+		expect(mike.search(2)).toBe("error");
 	});
 });
 
 describe("Deletes notes", function() {
+	var mike = new NotesApplication("mike");
 	it("Should take an integer", function() {
 		expect(typeof note_id).toBe(typeof(integer));
 		expect(delete("some")).toBeNaN();
@@ -46,6 +58,7 @@ describe("Deletes notes", function() {
 
 describe("Edits notes", function() {
 	it("Should take a string", function() {
+		var mike = new NotesApplication("mike");
 		expect(edit("from", 2)).toBe("error");
 		expect(edit(1, 3)).toBe("error");
 	});
